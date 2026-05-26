d20c1e00d88cea03bf13f481d6bd8cc4fbef768c
I Kadek Candra Gunawan <kadekcandra1630@gmail.com>
2026-05-26 23:44:03 +0800

    new commit

--- models/logistic_model.py
+++ models/logistic_model.py
@@ -17,19 +17,4 @@ class ToxicClassifier:
     
     def get_coefficients(self):
         return self.model.coef_[0]
-    class ToxicClassifier:
-    def __init__(self, max_iter=1000):
-        self.model = LogisticRegression(max_iter=max_iter, random_state=42)
-
-    def train(self, X_train_tfidf, y_train):
-        self.model.fit(X_train_tfidf, y_train)
-        return self.model
-
-    def predict(self, X_test_tfidf):
-        return self.model.predict(X_test_tfidf)
-
-    def evaluate(self, y_true, y_pred):
-        return classification_report(y_true, y_pred)
     
-    def get_coefficients(self):
-        return self.model.coef_[0]
